04c67b25248cd69afaf985fcbbbf2639d35fc8f8
ashokkumarsoliyappan <sashoksee@gmail.com>
2019-11-20 14:13:12 +0530

    excelwriter Latest

--- excelwriter.py
+++ excelwriter.py
@@ -1,19 +1,24 @@
-# import xlsxwriter
+import xlsxwriter
 
-# workbook = xlsxwriter.Workbook("Text.xlsx")
-# worksheet = workbook.add_worksheet()
+workbook = xlsxwriter.Workbook("GitHubUrl.xlsx")
+worksheet = workbook.add_worksheet()
 
-# #header format
-# boldFormat = workbook.add_format({'bold': True, 'font_color': 'red'})
-# boldFormat.set_align('center')
-# #header declarartion
-# worksheet.write('A1', 'File Name',boldFormat)
-# worksheet.write('B1', 'GitHub File URL',boldFormat)
+#header format
+titleFormat = workbook.add_format({'bold': True, 'font_color': 'red'})
+titleFormat.set_align('center')
+#header declarartion
+worksheet.write('A1', 'File Name',titleFormat)
+worksheet.write('B1', 'GitHub File URL',titleFormat)
 
 # for data in range(len(gitUrl)):
 	# worksheet.write(data+1,0,gitUrl[fileName])
 	# worksheet.write(data+1,1,values[gitUrl])
-# workbook.close()
-import subprocess
-label = subprocess.check_output(["git", "log -1"]).strip()
-print(label)
+workbook.close()
+
+# workbook = xlsxwriter.Workbook("GitHubUrl.xlsx")
+# worksheet = workbook.add_worksheet()
+
+
+# worksheet.write('A1', 'File Name',titleFormat)
+# worksheet.write('B1', 'GitHub File URL',titleFormat)
+# worksheet.write('C1', 'File Uploaded Date',titleFormat)
