9e978a673d06f35485836dab1c229de662b038f6
ashokkumarsoliyappan <sashoksee@gmail.com>
2019-11-20 11:23:18 +0530

    excelwriter Latest

---
+++ excelwriter.py
@@ -0,0 +1,16 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook("Text.xlsx")
+worksheet = workbook.add_worksheet()
+
+#header format
+boldFormat = workbook.add_format({'bold': True, 'font_color': 'red'})
+boldFormat.set_align('center')
+#header declarartion
+worksheet.write('A1', 'File Name',boldFormat)
+worksheet.write('B1', 'GitHub File URL',boldFormat)
+
+for data in range(len(gitUrl)):
+	worksheet.write(data+1,0,gitUrl[fileName])
+	worksheet.write(data+1,1,values[gitUrl])
+workbook.close()
