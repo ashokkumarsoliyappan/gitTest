18e49a05de24a16b5738fc33975ace3dcf1dbbc2
ashokkumarsoliyappan <sashoksee@gmail.com>
2019-11-20 12:30:19 +0530

    excelwriter Latest

--- excelwriter.py
+++ excelwriter.py
@@ -1,16 +1,19 @@
-import xlsxwriter
+# import xlsxwriter
 
-workbook = xlsxwriter.Workbook("Text.xlsx")
-worksheet = workbook.add_worksheet()
+# workbook = xlsxwriter.Workbook("Text.xlsx")
+# worksheet = workbook.add_worksheet()
 
-#header format
-boldFormat = workbook.add_format({'bold': True, 'font_color': 'red'})
-boldFormat.set_align('center')
-#header declarartion
-worksheet.write('A1', 'File Name',boldFormat)
-worksheet.write('B1', 'GitHub File URL',boldFormat)
+# #header format
+# boldFormat = workbook.add_format({'bold': True, 'font_color': 'red'})
+# boldFormat.set_align('center')
+# #header declarartion
+# worksheet.write('A1', 'File Name',boldFormat)
+# worksheet.write('B1', 'GitHub File URL',boldFormat)
 
-for data in range(len(gitUrl)):
-	worksheet.write(data+1,0,gitUrl[fileName])
-	worksheet.write(data+1,1,values[gitUrl])
-workbook.close()
+# for data in range(len(gitUrl)):
+	# worksheet.write(data+1,0,gitUrl[fileName])
+	# worksheet.write(data+1,1,values[gitUrl])
+# workbook.close()
+import subprocess
+label = subprocess.check_output(["git", "log -1"]).strip()
+print(label)
